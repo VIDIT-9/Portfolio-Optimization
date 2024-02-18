08ff268e0e2004808f273176c67b5a7a775719b9
Vidit Jain <102579972+VIDIT-9@users.noreply.github.com>
2024-02-19 02:08:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-
 import streamlit as st
+import os
+import pandas as pd
+from PIL import Image
 import pickle
-
 
 with open('final_allocation.pkl', 'rb') as file:
     final_allocation = pickle.load(file)
@@ -9,36 +10,37 @@ with open('final_allocation.pkl', 'rb') as file:
 with open('logo_list.pkl', 'rb') as logo:
     logo_list = pickle.load(logo)
 
-st.title("Portfolio Optimization App")
 
+st.title("Portfolio Optimization App")
 st.markdown('''
 **This app tries to find the optimal allocation of assets that aims to maximize returns while minimizing risk**
 ''')
 
-user_input = st.number_input("Amount to invest: ", min_value=100)
-
-if st.button("Optimize"):
+user_input = st.number_input("Amount to invest:", min_value=500)
 
 
-    if user_input:
+if st.button("Optimize") and user_input is not None:
+    
+    optimized_portfolio = final_allocation * user_input
+    optimized_portfolio = pd.DataFrame(optimized_portfolio)
 
-        optimized_portfolio = final_allocation * user_input
 
-        image_width = 100
-        image_height = 100
-        num_columns = 5
+    image_width = 50
+    image_height = 50
+    num_columns = 5
 
-        for i in range(0, len(logo_list), num_columns):
-            row = logo_list[i:i + num_columns]
-            row_values = optimized_portfolio.iloc[0,i:i + num_columns]
+    for i in range(2):
+        st.write("")  
+        row = st.columns(5) 
+        for j in range(5):
+            idx = i * 5 + j
+            ticker = optimized_portfolio.index[idx]
+            value = optimized_portfolio.iloc[idx, 0]
+
+            image_path = os.path.join('logo', f'{ticker}.png')
+            image = Image.open(image_path).resize((image_width, image_height))
             
-            columns = st.columns(num_columns)
-            
-            for j, ticker in enumerate(row):
-                value = row_values[j]
+            with row[j]:
+                st.image(image, caption=f'{ticker}', use_column_width=True)
+                st.write(f"Amount: {round(value)}")
                 
-                logo_url = f"https://logo.clearbit.com/{ticker}.com"
-                
-                with columns[j]:
-                    st.image(logo_url, width=image_width, use_column_width=False, output_format="auto")
-                    st.write(f"Amount: {round(value)}")
